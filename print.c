cbdffcece265b309995e1509feeaf32de155f0d3
Aisosa <allysonaghedo@gmail.com>
2022-05-21 10:20:27 +0100

    errors fixed

--- print.c
+++ print.c
@@ -2,12 +2,18 @@
 /**
  * _putchar - function to print a character
  * @c: character to be printed
+ * Return: 1
  */
 int _putchar(char c)
 {
 	write(1, &c, 1);
 	return (1);
 }
+/**
+ * _puts - prints out a string
+ * @s: string to be printed
+ * Return: string length
+ */
 int _puts(char *s)
 {
 	int i, size = strlen(s);
